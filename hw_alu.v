79754d07a16d8290b3888eb24f770ecac800c6f6
zaileshar <zaileshar@gmail.com>
2023-04-27 09:40:24 +0530

    fix timing constraints

---
+++ hw_alu.v
@@ -0,0 +1,10 @@
+module hw_alu (
+    input clk,
+    input rst_n,
+    output reg out
+);
+    always @(posedge clk) begin
+        if (!rst_n) out <= 0;
+        else out <= ~out;
+    end
+endmodule
