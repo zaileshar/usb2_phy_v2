96e3800c9a50af129b60cc1d47e31ade8339f67b
zaileshar <zaileshar@gmail.com>
2023-04-30 15:54:37 +0530

    fix css styles

---
+++ usb2_ctrl.v
@@ -0,0 +1,10 @@
+module usb2_ctrl (
+    input clk,
+    input rst_n,
+    output reg out
+);
+    always @(posedge clk) begin
+        if (!rst_n) out <= 0;
+        else out <= ~out;
+    end
+endmodule
